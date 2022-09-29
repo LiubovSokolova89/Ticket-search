62561c17d9d301f0fddd18fdd4bddb8465b94730
LiubovSokolova89 <liubow.sokolova@gmail.com>
2022-09-29 12:02:47 +0200

    Add test

--- src/test/java/ru/netology/manager/ManagerTest.java
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -1,26 +1,30 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.Ticket;
 import ru.netology.repository.Repository;
 
+import java.util.Arrays;
+
 public class ManagerTest {
 
-    Repository repo = new Repository();
-    Manager manager = new Manager(repo);
+    static Repository repo = new Repository();
+    static Manager manager = new Manager(repo);
 
-    Ticket ticket1 = new Ticket(1, 1000 , 60, "ALA" , "AAQ");
-    Ticket ticket2 = new Ticket(2, 1500, 80, "KRR", "GZP");
-    Ticket ticket3 = new Ticket(3, 200, 40, "AAQ", "KRR");
-    Ticket ticket4 = new Ticket(4, 5000, 120, "ARH", "ASF");
-    Ticket ticket5 = new Ticket(5, 3500, 100,"BXY", "BHX");
-    Ticket ticket6 = new Ticket(6, 750, 45, " BTK", "BZK");
-    Ticket ticket7 = new Ticket(7, 1300, 65, "VKT", "VOZ");
-    Ticket ticket8 = new Ticket(8, 7500, 180, "VKO", "GDZ");
+    static Ticket ticket1 = new Ticket(1, 1000 , 60, "ALA" , "AAQ");
+    static Ticket ticket2 = new Ticket(2, 1500, 80, "KRR", "GZP");
+    static Ticket ticket3 = new Ticket(3, 200, 40, "AAQ", "KRR");
+    static Ticket ticket4 = new Ticket(4, 5000, 120, "ARH", "ASF");
+    static Ticket ticket5 = new Ticket(5, 3500, 100,"BXY", "BHX");
+    static Ticket ticket6 = new Ticket(6, 750, 45, " BTK", "BZK");
+    static Ticket ticket7 = new Ticket(7, 1300, 65, "VKT", "VOZ");
+    static Ticket ticket8 = new Ticket(8, 7500, 180, "VKO", "GDZ");
 
-    @Test
-    public void ShouldFindAll() {
+
+    @BeforeAll
+    static void setUp() {
         repo.addTicket(ticket1);
         repo.addTicket(ticket2);
         repo.addTicket(ticket3);
@@ -29,11 +33,33 @@ public class ManagerTest {
         repo.addTicket(ticket6);
         repo.addTicket(ticket7);
         repo.addTicket(ticket8);
+    }
+
+    @Test
+    public void shouldFindAll() {
 
         Ticket[] expected = {ticket3};
         Assertions.assertArrayEquals(expected, manager.findAll("AAQ" , "KRR"));
     }
 
+    @Test
+    public void shouldSortByPrice() {
+
+        Ticket[] expected = new Ticket[] {
+                ticket3,
+                ticket6,
+                ticket1,
+                ticket7,
+                ticket2,
+                ticket5,
+                ticket4,
+                ticket8
+        };
+        Ticket[] actual = repo.findAll();
+
+        Arrays.sort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
 
 }
--- src/test/java/ru/netology/repository/RepositoryTest.java
+++ src/test/java/ru/netology/repository/RepositoryTest.java
@@ -1,28 +1,28 @@
 package ru.netology.repository;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.Ticket;
 import ru.netology.manager.Manager;
 
 public class RepositoryTest {
 
-    Repository repo = new Repository();
+    static Repository repo = new Repository();
+    static Manager manager = new Manager(repo);
 
 
-    Ticket ticket1 = new Ticket(1, 1000 , 60, "ALA" , "AAQ");
-    Ticket ticket2 = new Ticket(2, 1500, 80, "KRR", "GZP");
-    Ticket ticket3 = new Ticket(3, 200, 40, "AAQ", "KRR");
-    Ticket ticket4 = new Ticket(4, 5000, 120, "ARH", "ASF");
-    Ticket ticket5 = new Ticket(5, 3500, 100,"BXY", "BHX");
-    Ticket ticket6 = new Ticket(6, 750, 45, " BTK", "BZK");
-    Ticket ticket7 = new Ticket(7, 1300, 65, "VKT", "VOZ");
-    Ticket ticket8 = new Ticket(8, 7500, 180, "VKO", "GDZ");
+    static Ticket ticket1 = new Ticket(1, 1000 , 60, "ALA" , "AAQ");
+    static Ticket ticket2 = new Ticket(2, 1500, 80, "KRR", "GZP");
+    static Ticket ticket3 = new Ticket(3, 200, 40, "AAQ", "KRR");
+    static Ticket ticket4 = new Ticket(4, 5000, 120, "ARH", "ASF");
+    static Ticket ticket5 = new Ticket(5, 3500, 100,"BXY", "BHX");
+    static Ticket ticket6 = new Ticket(6, 750, 45, " BTK", "BZK");
+    static Ticket ticket7 = new Ticket(7, 1300, 65, "VKT", "VOZ");
+    static Ticket ticket8 = new Ticket(8, 7500, 180, "VKO", "GDZ");
 
-
-
-    @Test
-    public void removeById() {
+    @BeforeAll
+    static void setUp() {
         repo.addTicket(ticket1);
         repo.addTicket(ticket2);
         repo.addTicket(ticket3);
@@ -30,7 +30,13 @@ public class RepositoryTest {
         repo.addTicket(ticket5);
         repo.addTicket(ticket6);
         repo.addTicket(ticket7);
-        repo.addTicket(ticket8);
+
+    }
+
+
+
+    @Test
+    public void removeById() {
 
         repo.removeById(3);
         Ticket[] actual = repo.findAll();
@@ -40,9 +46,26 @@ public class RepositoryTest {
                 ticket4,
                 ticket5,
                 ticket6,
-                ticket7,
-                ticket8
+                ticket7
         };
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindAll() {
+
+        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7,};
+        Ticket[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAdd() {
+        repo.addTicket(ticket8);
+        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8,};
+        Ticket[] actual = repo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
