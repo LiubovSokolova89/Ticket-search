cf11d07b2cf087087cad59790a4a0d03bae43aa2
LiubovSokolova89 <liubow.sokolova@gmail.com>
2022-10-04 12:42:08 +0200

    Add test

--- src/test/java/ru/netology/manager/ManagerTest.java
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -8,6 +8,7 @@ import ru.netology.repository.Repository;
 
 import java.util.Arrays;
 
+
 public class ManagerTest {
 
     static Repository repo = new Repository();
@@ -16,8 +17,8 @@ public class ManagerTest {
     static Ticket ticket1 = new Ticket(1, 1000, 60, "ALA", "AAQ");
     static Ticket ticket2 = new Ticket(2, 1500, 80, "KRR", "GZP");
     static Ticket ticket3 = new Ticket(3, 200, 40, "AAQ", "KRR");
-    static Ticket ticket4 = new Ticket(4, 5000, 120, "ARH", "ASF");
-    static Ticket ticket5 = new Ticket(5, 3500, 100, "BXY", "BHX");
+    static Ticket ticket4 = new Ticket(4, 5000, 120, "VKO", "GDZ");
+    static Ticket ticket5 = new Ticket(5, 3500, 100, "VKO", "KRR");
     static Ticket ticket6 = new Ticket(6, 750, 45, " BTK", "BZK");
     static Ticket ticket7 = new Ticket(7, 1300, 65, "VKT", "VOZ");
     static Ticket ticket8 = new Ticket(8, 7500, 180, "VKO", "GDZ");
@@ -36,10 +37,24 @@ public class ManagerTest {
     }
 
     @Test
-    public void shouldFindAll() {
+    public void shouldFindOneTicket() {
 
         Ticket[] expected = {ticket3};
         Assertions.assertArrayEquals(expected, manager.findAll("AAQ", "KRR"));
+    }
+
+    @Test
+    public void shouldFindFewTicket() {
+        Ticket[] expected = {ticket4, ticket8};
+        Ticket[] actual = manager.findAll("VKO","GDZ");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindZeroTicket() {
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("VKO", "AAQ");
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
--- src/test/java/ru/netology/repository/RepositoryTest.java
+++ src/test/java/ru/netology/repository/RepositoryTest.java
@@ -15,8 +15,8 @@ public class RepositoryTest {
     static Ticket ticket1 = new Ticket(1, 1000, 60, "ALA", "AAQ");
     static Ticket ticket2 = new Ticket(2, 1500, 80, "KRR", "GZP");
     static Ticket ticket3 = new Ticket(3, 200, 40, "AAQ", "KRR");
-    static Ticket ticket4 = new Ticket(4, 5000, 120, "ARH", "ASF");
-    static Ticket ticket5 = new Ticket(5, 3500, 100, "BXY", "BHX");
+    static Ticket ticket4 = new Ticket(4, 5000, 120, "VKO", "GDZ");
+    static Ticket ticket5 = new Ticket(5, 3500, 100, "VKO", "KRR");
     static Ticket ticket6 = new Ticket(6, 750, 45, " BTK", "BZK");
     static Ticket ticket7 = new Ticket(7, 1300, 65, "VKT", "VOZ");
     static Ticket ticket8 = new Ticket(8, 7500, 180, "VKO", "GDZ");
